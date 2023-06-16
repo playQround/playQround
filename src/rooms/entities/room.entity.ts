fa2cd414e65b56309ccbfdf993f76701c5cc528f
Chunws13 <dntkd51@naver.com>
2023-06-16 20:11:00 +0900

    first test commit

--- src/rooms/entities/room.entity.ts
+++ src/rooms/entities/room.entity.ts
@@ -1,1 +1,16 @@
-export class Room {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Rooms {
+    @PrimaryGeneratedColumn()
+    roomId : number;
+    key : string;
+    roomName : string;
+    roomStatus : number;
+    maxPeople : number;
+    nowPeople : number;
+    public : boolean;
+    cutRationg : number;
+    createdAt: Date;
+    updatedAt : Date;
+}
