ca78fc4ea371678c60e3e7affb287b179299669b
Chunws13 <dntkd51@naver.com>
2023-06-17 15:06:55 +0900

    방 수정 & 삭제 기능 추가

--- src/rooms/dto/update-room.dto.ts
+++ src/rooms/dto/update-room.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateRoomDto } from "./create-room.dto";
 
-export class UpdateRoomDto extends PartialType(CreateRoomDto) {}
+export class UpdateRoomDto extends PartialType(CreateRoomDto) {
+    readonly maxPeople?: number;
+    readonly cutRating?: number;
+}
