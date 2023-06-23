038e45cda9343a77b53f4bea970134a72081b982
kkerast <dlwlsgkqkq@naver.com>
2023-06-23 21:36:45 +0900

    퀴즈 중간저장 구현 test 폴더에 html 파일 추가

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { join } from "path";
 async function bootstrap() {
     //const app = await NestFactory.create(AppModule);
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    console.log(join(__dirname, "../.."));
-    app.useStaticAssets(join(__dirname, "../.."));
+    console.log(join(__dirname, "../test"));
+    app.useStaticAssets(join(__dirname, "../test"));
     app.setGlobalPrefix("api");
     await app.listen(3000);
 }
