83d9c228f17fa4aa4e7fe44098d024bb3c8d1a61
kkerast <dlwlsgkqkq@naver.com>
2023-07-01 20:57:02 +0900

    퀴즈 준비시간 추가 및 중간결과 저장부분 수정

--- src/records/records.service.ts
+++ src/records/records.service.ts
@@ -1,20 +1,36 @@
-import { Injectable, NotFoundException, Logger } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 // import { CreateRoomDto } from "./dto/create-room.dto";
 import { UpdateRecordDto } from "./dto/update-record.dto";
 
 import { RecordsRepository } from "./records.repository";
+import { json } from "stream/consumers";
 
 @Injectable()
 export class RecordsService {
-    // Logger 사용
-    private readonly logger = new Logger(RecordsService.name);
     // 사용할 DB table 불러오기
     constructor(private readonly RecordsRepository: RecordsRepository) {}
 
     async update(UpdateRecordDto: UpdateRecordDto): Promise<object> {
-        this.logger.verbose(
-            `Updating record for user: ${UpdateRecordDto?.userId} in room: ${UpdateRecordDto?.roomId}. New score: ${UpdateRecordDto?.userScore}`,
-        );
         return await this.RecordsRepository.update(UpdateRecordDto);
+    }
+
+    async getRoomRecord(roomId: number): Promise<string> {
+        const record = await this.RecordsRepository.getRoomRecord(roomId);
+
+        // object 형식의 record 를 json 형식으로 변환
+        const jsonRecord = JSON.parse(JSON.stringify(record));
+        // jsonRecord 를 userScore로 정렬하고 username과 userScore값만 가지는 배열로 변환
+        const sortedRecord = jsonRecord
+            .sort((a, b) => b.userScore - a.userScore)
+            .map((record) => {
+                return {
+                    username: record.userName,
+                    userScore: record.userScore,
+                };
+            });
+        //sortedRecord를 string으로 변환
+        const stringRecord = JSON.stringify(sortedRecord);
+
+        return stringRecord;
     }
 }
