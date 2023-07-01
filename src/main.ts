9a4c2c7da8b59076d480c3518e647f19b6fee6d8
peterlah <peterlah@naver.com>
2023-07-01 19:58:56 +0900

    포트 수정

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe()); // 입력값 유효성 검사를 위한 ValidationPipe 추가
     app.setGlobalPrefix("api");
-    const port = 3000;
+    const port = 80;
     await app.listen(port);
     Logger.log(`Application is running on port(${port})`);
 }
