c81f209b76bd34b256477f4a35c317aaf77e542d
Juhyuck-Hong <juhyukhong@gmail.com>
2023-07-08 16:58:14 +0900

    webRTC 모듈 삭제

--- src/webrtc/webrtc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { webRtcGateway } from "./webrtc.gateway";
-
-@Module({
-    imports: [],
-    providers:[webRtcGateway]
-})
-
-export class webrtcMoudle{}
