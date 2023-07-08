a6668144467974b6300b7b8946ee774e896c87c7
peterlah <58283954+peterlah@users.noreply.github.com>
2023-07-08 20:22:50 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-    // const app = await NestFactory.create<NestExpressApplication>(
     const app = await NestFactory.create<NestExpressApplication>(
         AppModule, 
         { cors : true }
