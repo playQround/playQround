293dbb44ba3ac70073113e3c37d4ac849b43632c
J Hong <128202933+JuhyuckHong@users.noreply.github.com>
2023-07-18 00:17:04 +0900

    Update users.repository.ts

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Users } from "./entities/user.entity";
-import { Repository, TreeRepositoryUtils } from "typeorm";
+import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
