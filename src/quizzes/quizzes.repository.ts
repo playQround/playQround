bba5546f4a2388e31a08d0573fcc1ac943cc271a
Juhyuck-Hong <juhyukhong@gmail.com>
2023-07-18 08:31:41 +0900

    try-catch 전체 감싸기

--- src/quizzes/quizzes.repository.ts
+++ src/quizzes/quizzes.repository.ts
@@ -27,44 +27,12 @@ export class QuizzesRepository {
         @Inject(CACHE_MANAGER) private cacheManager: Cache, // 캐시를 사용하기 위해 추가
     ) {}
 
-    async start(id: string, quizCount: number): Promise<any> {
-        // utils 로 보낼 함수
-        const generateRandomQuizzes = async (quizCount: number) => {
-            let quizArray = [];
-            let checker = {};
-            let randomQuiz: number;
-
-            while (quizArray.length < quizCount) {
-                // 아래 1000 값은 db 길이로 반환해야함.
-                const count: number = await this.getQuizCount();
-                randomQuiz = Math.floor(Math.random() * (count - 1)) + 1;
-
-                // 이미 들어간 값인지 검사하면서 퀴즈 리스트에 넣음
-                if (!checker[randomQuiz]) {
-                    checker[randomQuiz] = 1;
-                    quizArray.push(randomQuiz);
-                }
-            }
-
-            return quizArray;
-        };
-
-        const quizIdList = await generateRandomQuizzes(quizCount);
-
-        const quizList = await this.quizzesRepository
-            .createQueryBuilder("quizzes")
-            .where("quizzes.quizid IN (:id)", { id: quizIdList })
-            .getMany();
-
-        return { quizzes: quizList };
-    }
-
     //퀴즈 DB에서 quizId 기준으로 퀴즈를 찾는다. (Redis Cache 적용)
     async startQuiz(quizId: number): Promise<Quizzes> {
-        this.logger.info(
-            "quizzes repository startQuiz, before find cachedQuiz",
-        );
         try {
+            this.logger.info(
+                "quizzes repository startQuiz, before find cachedQuiz",
+            );
             // 캐시에서 해당 퀴즈를 찾는다.
             const cachedQuiz = await this.cacheManager.get<Quizzes>(
                 `quiz_${quizId}`,
@@ -78,53 +46,34 @@ export class QuizzesRepository {
             );
         }
 
-        this.logger.info(
-            "quizzes repository startQuiz, if there is no cachedQuiz, find one",
-        );
-        // 캐시에 해당 퀴즈가 없는 경우, DB에서 모든 퀴즈를 찾아 캐시에 넣는다.
-        const quiz = await this.quizzesRepository.findOne({
-            where: { quizid: quizId },
-        });
-        this.logger.info("quizzes repository startQuiz, save a quiz to cache");
         try {
+            this.logger.info(
+                "quizzes repository startQuiz, if there is no cachedQuiz, find one",
+            );
+            // 캐시에 해당 퀴즈가 없는 경우, DB에서 모든 퀴즈를 찾아 캐시에 넣는다.
+            const quiz = await this.quizzesRepository.findOne({
+                where: { quizid: quizId },
+            });
+            this.logger.info(
+                "quizzes repository startQuiz, save a quiz to cache",
+            );
             await this.cacheManager.set(`quiz_${quiz.quizid}`, quiz, 3000000); // Cache for 50 minutes
+            this.logger.info("quizzes repository startQuiz, return");
+            return quiz;
         } catch (error) {
             this.logger.info(
                 `quizzes repository startQuiz, save a quiz to cache while error with ${error}`,
             );
         }
-
-        this.logger.info("quizzes repository startQuiz, return");
-        return quiz;
     }
-
-    // //퀴즈 DB에서 quizId 기준으로 퀴즈를 찾는다. (Redis Cache 적용)
-    // async startQuiz(quizId: number): Promise<Quizzes> {
-    //     // 캐시에서 해당 퀴즈를 찾는다.
-    //     const cachedQuiz = await this.cacheManager.get<Quizzes>(
-    //         `quiz_${quizId}`,
-    //     );
-    //     if (cachedQuiz) {
-    //         return cachedQuiz;
-    //     }
-
-    //     const quizCount = await this.getQuizCount();
-    //     const randomNum = Math.floor(Math.random() * quizCount) + 1;
-    //     const quiz = await this.quizzesRepository.findOne({
-    //         where: {
-    //             quizid: randomNum,
-    //         },
-    //     });
-    //     await this.cacheManager.set(`quiz_${quiz.quizid}`, quiz, 3000); // Cache for 50 minutes
-
-    //     return quiz;
-    // }
 
     //퀴즈 DB의 총 갯수를 구한다. (Redis Cache 적용)
     async getQuizCount(): Promise<number> {
-        this.logger.info("quizzes repository getQuizCount before cachedCount");
-        // 레디스 캐시 서버에서 문제 총 개수 조회
         try {
+            this.logger.info(
+                "quizzes repository getQuizCount before cachedCount",
+            );
+            // 레디스 캐시 서버에서 문제 총 개수 조회
             const cachedCount = await this.cacheManager.get<number>(
                 "quizCount",
             );
@@ -137,17 +86,18 @@ export class QuizzesRepository {
             );
         }
 
-        this.logger.info("quizzes repository getQuizCount after cachedCount");
-        // 레디스 캐시 서버에 문제 총 개수에 대한 정보가 없는 경우 데이터베이스에서 직접 조회 및 해당 값을 캐시 서버에 등록
-        const count = await this.quizzesRepository.count();
         try {
+            this.logger.info(
+                "quizzes repository getQuizCount after cachedCount",
+            );
+            // 레디스 캐시 서버에 문제 총 개수에 대한 정보가 없는 경우 데이터베이스에서 직접 조회 및 해당 값을 캐시 서버에 등록
+            const count = await this.quizzesRepository.count();
             this.cacheManager.set("quizCount", count, 3000000); // Cache for 50 minutes
+            return count;
         } catch (error) {
             this.logger.info(
                 `quizzes repository getQuizCount after cachedCount while error with ${error}`,
             );
         }
-
-        return count;
     }
 }
--- src/quizzes/quizzes.service.ts
+++ src/quizzes/quizzes.service.ts
@@ -15,7 +15,11 @@ export class QuizzesService {
             winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
             winston.format.json(),
         ),
-        transports: [new winston.transports.File({ filename: join(__dirname, "../../test/info.log") })],
+        transports: [
+            new winston.transports.File({
+                filename: join(__dirname, "../../test/info.log"),
+            }),
+        ],
     });
     constructor(
         private readonly quizzesRepository: QuizzesRepository,
@@ -24,74 +28,106 @@ export class QuizzesService {
 
     //정답을 체크한다. 유저가 입력한 message와 answer 정답을 비교 정답이면 true 오답이면 false를 반환한다.
     async checkAnswer(message: string, room: string): Promise<boolean> {
-        const roomInfo = await this.roomsRepository.findOne(room);
-        //console.log("answer_check", roomInfo);
-        //console.log("Message", message);
-        //console.log("answer_check", roomInfo["nowAnswer"]);
-        if (message === roomInfo["nowAnswer"]) {
-            return true;
+        try {
+            const roomInfo = await this.roomsRepository.findOne(room);
+            //console.log("answer_check", roomInfo);
+            //console.log("Message", message);
+            //console.log("answer_check", roomInfo["nowAnswer"]);
+            if (message === roomInfo["nowAnswer"]) {
+                return true;
+            }
+            return false;
+        } catch (error) {
+            return false;
         }
-        return false;
     }
 
     // 퀴즈 조회 서비스
     async getQuiz(): Promise<any> {
-        // 퀴즈 DB의 총 갯수를 구한다.
-        this.logger.info("quizzes service, call quizzes repository getQuizCount")
-        const quizCount = await this.quizzesRepository.getQuizCount();
+        try {
+            // 퀴즈 DB의 총 갯수를 구한다.
+            this.logger.info(
+                "quizzes service, call quizzes repository getQuizCount",
+            );
+            const quizCount = await this.quizzesRepository.getQuizCount();
 
-        this.logger.info("quizzes service, make random number")
-        //랜덤한 id값을 생성하고 그 id값의 퀴즈를 고른다.
-        const randomNum = Math.floor(Math.random() * quizCount) + 1;
+            this.logger.info("quizzes service, make random number");
+            //랜덤한 id값을 생성하고 그 id값의 퀴즈를 고른다.
+            const randomNum = Math.floor(Math.random() * quizCount) + 1;
 
-        this.logger.info("quizzes service, call quizzes repository startQuiz")
-        // 퀴즈 DB에서 quizId를 기준으로 퀴즈를 찾는다.
-        const newQuiz = await this.quizzesRepository.startQuiz(randomNum);
-        return newQuiz;
+            this.logger.info(
+                "quizzes service, call quizzes repository startQuiz",
+            );
+            // 퀴즈 DB에서 quizId를 기준으로 퀴즈를 찾는다.
+            const newQuiz = await this.quizzesRepository.startQuiz(randomNum);
+            return newQuiz;
+        } catch (error) {
+            this.logger.info(
+                `quizzes service, error in quizzes service getQuiz ${error}`,
+            );
+        }
     }
 
     updateRoomAnswer(roomId: number, answer: string): Promise<void> {
-        this.roomsRepository.updateRoomAnswer(roomId, answer);
-        this.logger.info("quizzes service, update room answer to rooms repository")
-        return;
+        try {
+            this.roomsRepository.updateRoomAnswer(roomId, answer);
+            this.logger.info(
+                "quizzes service, update room answer to rooms repository",
+            );
+            return;
+        } catch (error) {
+            this.logger.info(
+                `quizzes service, update room answer with error: ${error}`,
+            );
+        }
     }
 
     async updateCombo(roomId: number, userId: number) {
-        //룸 아이디와 유저아이디 값으로 콤보를 업데이트한다.
-        const comboData = await this.roomsRepository.updateCombo(
-            roomId,
-            userId,
-        );
-        console.log("comboData", comboData);
+        try {
+            //룸 아이디와 유저아이디 값으로 콤보를 업데이트한다.
+            const comboData = await this.roomsRepository.updateCombo(
+                roomId,
+                userId,
+            );
+            console.log("comboData", comboData);
 
-        const comboPoint = [0, 0, 1, 3, 5, 10];
-        const comboName = [
-            ``, //0
-            ``, //1
-            `더블 +1`, //2
-            `트리플 +3`, //3
-            `쿼드라 +5`, //4
-            `펜타 +10`, //5
-            `전설의 출현 +10`, //6이상
-        ];
-        //콤보가 6이상이면 전설의 출현이라는 메세지를 보낸다.
-        const comboMention =
-            comboData["combo"] < 6
-                ? comboName[+comboData["combo"]]
-                : comboName[6];
-        //삼항연산자로 할당
-        const nowPoint =
-            comboData["combo"] < 5 ? comboPoint[+comboData["combo"]] : 10;
+            const comboPoint = [0, 0, 1, 3, 5, 10];
+            const comboName = [
+                ``, //0
+                ``, //1
+                `더블 +1`, //2
+                `트리플 +3`, //3
+                `쿼드라 +5`, //4
+                `펜타 +10`, //5
+                `전설의 출현 +10`, //6이상
+            ];
+            //콤보가 6이상이면 전설의 출현이라는 메세지를 보낸다.
+            const comboMention =
+                comboData["combo"] < 6
+                    ? comboName[+comboData["combo"]]
+                    : comboName[6];
+            //삼항연산자로 할당
+            const nowPoint =
+                comboData["combo"] < 5 ? comboPoint[+comboData["combo"]] : 10;
 
-        //콤보 점수도 계산해서 object로 반환한다.
-        const comboObject = {
-            combo: comboData["combo"],
-            lastAnswerUserId: comboData["lastAnswerUserId"],
-            comboPoint: nowPoint,
-            comboMention,
-        };
-        console.log("comboObject", comboObject);
-        return comboObject;
+            //콤보 점수도 계산해서 object로 반환한다.
+            const comboObject = {
+                combo: comboData["combo"],
+                lastAnswerUserId: comboData["lastAnswerUserId"],
+                comboPoint: nowPoint,
+                comboMention,
+            };
+            console.log("comboObject", comboObject);
+            return comboObject;
+        } catch (error) {
+            const comboObject = {
+                combo: 2,
+                lastAnswerUserId: "",
+                comboPoint: 0,
+                comboMention: "콤보 에러 발생",
+            };
+            return comboObject;
+        }
     }
 
     create(createQuizDto: CreateQuizDto) {
