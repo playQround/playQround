a5325513b858fe5409bdbeaa6f114ba022000ad9
kkerast <dlwlsgkqkq@naver.com>
2023-07-22 20:20:57 +0900

    유저의 총 맞춘 문제 최고 콤보수 추가

--- src/records/schema/records.schema.ts
+++ src/records/schema/records.schema.ts
@@ -15,6 +15,12 @@ export class Record {
     userName: string;
     @Prop()
     userScore: number;
+    //default 0 nowCorrect 값은 정답을 맞춘 횟수를 의미한다.
+    @Prop({ default: 0 })
+    nowCorrect: number;
+    //default 0 maxCombo 값은 현재 게임에서 최고 콤보 숫자를 의미한다.
+    @Prop({ default: 0 })
+    maxCombo: number;
 }
 
 export const RecordSchema = SchemaFactory.createForClass(Record);
